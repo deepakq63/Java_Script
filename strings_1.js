0eebe713918ae007c14aec30add82051b0c3284c
Deepak <120923265+deepakq63@users.noreply.github.com>
2023-01-01 00:44:58 +0530

    Add files via upload

---
+++ strings_1.js
@@ -0,0 +1,19 @@
+let name = "Yuuri"
+// console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let friend = 'Haruto'
+console.log(friend)
+
+// Template literals
+let boy1 = "Neji"
+let boy2 = "Kenji"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+let fruit = "Bana\"na"
+console.log(fruit)
---
+++ strings_2.js
@@ -0,0 +1,20 @@
+let name = "Yurri"
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2, 4)) will include 2 but not 4
+// console.log(name.slice(2))
+// console.log(name.replace("Yu", "Ze"))
+// let friend = "Nezuko"
+// console.log(name.concat(" is a friend of ", friend, " Ok"))
+// let friend2 = "      Zenistu        "
+// console.log(friend2)
+// console.log(friend2.trim())
+let fr = "Aoto" + "Hinata" + "Akari"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+// fr[4]= "o" // This is not possible 
+console.log(fr)
